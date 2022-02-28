496d020cf76da6e479f87e0a1c83da74b1d81093
Jules PELLETIER <julpelle@e1r4p20.42.fr>
2022-02-28 15:41:18 +0100

    06 ready for push

--- CPP06/ex00/srcs/main.cpp
+++ CPP06/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: julpelle <julpelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 20:11:57 by julpelle          #+#    #+#             */
-/*   Updated: 2022/02/28 10:52:12 by julpelle         ###   ########.fr       */
+/*   Updated: 2022/02/28 15:39:11 by julpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,10 @@ int main(int ac, char **av)
 		std::cout << "Error : you need to input arguments to convert " << std::endl;
 	else
 	{
+		std::cout << " --- Printing original test --- " << std::endl;
+		Convert conv1("0");
+		Convert conv2("42.0f");
+		Convert conv3("nan");
 		while (av[i])
 		{
 			Convert	conv(av[i]);
